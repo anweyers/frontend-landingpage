ed08f650bd11844617e18326014615e5ef8d86bd
Anette <an.weyers@web.de>
2020-01-05 17:27:44 +0100

    Add: Navigation

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-const listOfSections = document.querySelectorAll('section');
+/*const listOfSections = document.querySelectorAll('section');*/
+const sections = document.getElementsByTagName('section');
 const unorderedList = document.getElementById('navbar__list');
 
 
@@ -38,28 +39,67 @@ function isScrolledIntoView(el) {
   isVisible = elemTop < window.innerHeight && elemBottom >= 0;
   return isVisible;
 }
-
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+/************* */
+iterate();
+
+function iterate() {
 /*** Iterieren durch die sections, falls im Viewport class=active hinzufügen ****/
-for (let i=0; i<listOfSections.length; i++)  {
-  console.log(listOfSections[i]);
-  let sectionInView = isScrolledIntoView(listOfSections[i]);
+  for (i=0; i<sections.length; i++)  {
+   console.log(sections[i]);
+   readSectionName();
+   setSectionActive();
+  }
+}
+
+function readSectionName() {
+  const currentSection = sections[i];
+  const sectionName = currentSection.getAttribute('data-nav');
+  console.log('current Section: ' +sectionName); ///Ausgabe Section 1/2/3
+  buildNav(sectionName);
+}
+ 
+
+function buildNav(secName) {
+  let listitem = document.createElement('li');
+  listitem.classList.add('menu__link', sections[i].getAttribute('id'));
+  let link = document.createElement('a');
+  link.textContent = secName;
+  listitem.appendChild(link);
+  unorderedList.appendChild(listitem);
+}
+
+function setSectionActive() {
+  let sectionInView = isScrolledIntoView(sections[i]); //true or false
   console.log("SectionInView? "+ sectionInView);
   if (sectionInView === true) {
-    listOfSections[i].classList.add("active");
-    console.log("added class-active to "+listOfSections[i])
+    sections[i].classList.add("active"); // falls sichtbar class = active setzen
+    console.log("added class-active to "+sections[i].id);
   } 
 }
 
-// build the nav
-for (let i=0; i<listOfSections.length; i++) { 
-  const container = document.createElement('li');
-  unorderedList.appendChild(container);
-  }
+  /*let sectionInView = isScrolledIntoView(sections[i]); //true or false
+  console.log("SectionInView? "+ sectionInView);
+  if (sectionInView === true) {
+    sections[i].classList.add("active"); // falls sichtbar class = active setzen
+    console.log("added class-active to "+sections[i].id);
+  } 
+}*/
+
+
+
+/*const mainHeading = document.querySelector('li');
+  mainHeading.addEventListener('click', function () {
+    console.log('The heading was clicked!');
+  }); */
+
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
