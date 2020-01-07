f04f088cc38e3ccecd82db84b3755aca480cd4a9
Anette <an.weyers@web.de>
2020-01-07 23:25:04 +0100

    Finished functionality, need to refactor

--- js/app.js
+++ js/app.js
@@ -1,70 +1,22 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Define Global Variables
- * 
-*/
-/*const listOfSections = document.querySelectorAll('section');*/
 const sections = document.getElementsByTagName('section');
 const unorderedList = document.getElementById('navbar__list');
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-/*** Hilfsfkt ***/
-function isScrolledIntoView(el) {
+function isVisibleInView(el) {
   var rect = el.getBoundingClientRect();
   var elemTop = rect.top;
   var elemBottom = rect.bottom;
-
-  // Only completely visible elements return true:
-  //var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-  // Partially visible elements return true:
   isVisible = elemTop < window.innerHeight && elemBottom >= 0;
   return isVisible;
 }
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-/************* */
-iterate();
 
-function iterate() {
-/*** Iterieren durch die sections, falls im Viewport class=active hinzufügen ****/
-  for (i=0; i<sections.length; i++)  {
-   console.log(sections[i]);
-   readSectionName();
-   setSectionActive();
-
-  }
-}
-
-function readSectionName() {
+/* begin */
+for (i=0; i<sections.length; i++)  {
   const currentSection = sections[i];
   const sectionName = currentSection.getAttribute('data-nav');
-  console.log('current Section: ' +sectionName); ///Ausgabe Section 1/2/3
   buildNav(sectionName);
 }
- 
-
 function buildNav(secName) {
   let listitem = document.createElement('li');
   listitem.classList.add('menu__link', sections[i].getAttribute('id'));
@@ -72,44 +24,41 @@ function buildNav(secName) {
   link.textContent = secName;
   listitem.appendChild(link);
   unorderedList.appendChild(listitem);
-
-  // which listItem was clicked, read id :-)
-  listitem.addEventListener('click', function() {
-    let elemId=listitem.classList[1];
-    console.log(secName +' item, id: '+elemId+' was clicked!');
-    //move smoothly to id corresponding section
-    document.getElementById(elemId).scrollIntoView({
-          behavior: 'smooth'
-    });
-  });    
+  clickAndScroll(listitem);
 }
 
+//click event
+function clickAndScroll (liItem) {
+    let sectionInView = isVisibleInView(sections[i]); //true or false
+    console.log(sections[i].id+" SectionInView? "+ sectionInView); 
+
+    liItem.addEventListener('click', function() {
+    //read li and move smoothly to the id corresponding section
+    let elemId=liItem.classList[1];
+    console.log("you clicked li "+elemId);
+    let activeSection=document.getElementById(elemId); // section elem
+    
+      for (const section of sections)  {
+        console.log(section.id);
+        section.classList.remove("active");
+    }
+    activeSection.classList.add("active");
+    scroll(elemId);
+  });    
+
+  function scroll(elemId) {
+    document.getElementById(elemId).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+}
 function setSectionActive() {
   let sectionInView = isScrolledIntoView(sections[i]); //true or false
   console.log("SectionInView? "+ sectionInView);
   if (sectionInView === true) {
-    sections[i].classList.add("active"); // falls sichtbar class = active setzen
     console.log("added class-active to "+sections[i].id);
   } 
+  else {
+   console.log("removed class-active to "+sections[i].id);
+  }
 }
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
