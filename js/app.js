41f2ea7a6bf20776d508f9687de15717fee2bbd9
Anette <an.weyers@web.de>
2020-01-08 00:42:50 +0100

    refactor: cleanout functionality and code structuring

--- js/app.js
+++ js/app.js
@@ -2,63 +2,45 @@
 const sections = document.getElementsByTagName('section');
 const unorderedList = document.getElementById('navbar__list');
 
-
+/*Helper*/
 function isVisibleInView(el) {
-  var rect = el.getBoundingClientRect();
-  var elemTop = rect.top;
-  var elemBottom = rect.bottom;
-  isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-  return isVisible;
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+    isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
 }
 
-/* begin */
+/* read amount of sections */
 for (i=0; i<sections.length; i++)  {
-  const currentSection = sections[i];
-  const sectionName = currentSection.getAttribute('data-nav');
-  buildNav(sectionName);
-}
-function buildNav(secName) {
-  let listitem = document.createElement('li');
-  listitem.classList.add('menu__link', sections[i].getAttribute('id'));
-  let link = document.createElement('a');
-  link.textContent = secName;
-  listitem.appendChild(link);
-  unorderedList.appendChild(listitem);
-  clickAndScroll(listitem);
+    const currentSection = sections[i];
+    const sectionName = currentSection.getAttribute('data-nav');
+    buildNav(sectionName);
 }
 
-//click event
-function clickAndScroll (liItem) {
-    let sectionInView = isVisibleInView(sections[i]); //true or false
-    console.log(sections[i].id+" SectionInView? "+ sectionInView); 
+/* create navigation items */
+function buildNav(sectionName) {
+    let listitem = document.createElement('li');
+    let link = document.createElement('a');
+    listitem.classList.add('menu__link', sections[i].getAttribute('id'));
+    link.textContent = sectionName;
+    listitem.appendChild(link);
+    unorderedList.appendChild(listitem);
+    clickAndScroll(listitem);
+}
 
-    liItem.addEventListener('click', function() {
-    //read li and move smoothly to the id corresponding section
-    let elemId=liItem.classList[1];
-    console.log("you clicked li "+elemId);
-    let activeSection=document.getElementById(elemId); // section elem
-    
-      for (const section of sections)  {
-        console.log(section.id);
-        section.classList.remove("active");
-    }
-    activeSection.classList.add("active");
-    scroll(elemId);
-  });    
-
-  function scroll(elemId) {
-    document.getElementById(elemId).scrollIntoView({
-      behavior: 'smooth'
+/* click event handling */
+function clickAndScroll (listItem) {
+    let sectionInView = isVisibleInView(sections[i]);
+    listItem.addEventListener('click', function() {
+        let elemId=listItem.classList[1];
+        let activeSection=document.getElementById(elemId);
+        for (const section of sections)  {
+            section.classList.remove("active");
+        }
+        activeSection.classList.add("active");
+        document.getElementById(elemId).scrollIntoView({
+            behavior: 'smooth'
+      });
     });
-  }
-}
-function setSectionActive() {
-  let sectionInView = isScrolledIntoView(sections[i]); //true or false
-  console.log("SectionInView? "+ sectionInView);
-  if (sectionInView === true) {
-    console.log("added class-active to "+sections[i].id);
-  } 
-  else {
-   console.log("removed class-active to "+sections[i].id);
-  }
 }
